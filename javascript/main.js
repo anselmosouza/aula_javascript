99975f909a69d75d5f2401ff4891a279e8e206b6
Anselmo Souza <anselmo.souza@ba.senac.br>
2020-07-20 16:59:20 -0300

    Primeiro exercicios

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+var nome = "Anselmo Ferreira";
+var n1 = 3;
+var n2 = 5;
+var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos")
+
+//alert(idade + idade2)
+
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase);
+console.log(frase.toLowerCase());
+//alert(frase.replace("Japão", "Brasil"))
